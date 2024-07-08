bbe8d26bd2ddec75e8d7620b5594b8a3e9fa08bc
Anish <basketb523@gmail.com>
2024-07-08 14:32:49 +0545

    suspense component load for all components

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,10 +1,10 @@
-import { Card } from "../ui/dashboard/cards";
-import { lusitana } from "../ui/fonts";
-import RevenueChart from "../ui/dashboard/revenue-chart";
-import LatestInvoices from "../ui/dashboard/latest-invoices";
-import { fetchLatestInvoices, fetchCardData } from "../lib/data";
+import CardWrapper from "@/app/ui/dashboard/cards";
+import { lusitana } from "../../ui/fonts";
+import RevenueChart from "../../ui/dashboard/revenue-chart";
+import LatestInvoices from "../../ui/dashboard/latest-invoices";
+import { fetchLatestInvoices, fetchCardData } from "../../lib/data";
 import { Suspense } from "react";
-import { RevenueChartSkeleton, LatestInvoicesSkeleton } from "../ui/skeletons";
+import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from "../../ui/skeletons";
 export default async function Page() {
   // const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
@@ -18,14 +18,9 @@ export default async function Page() {
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-        <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        />
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
